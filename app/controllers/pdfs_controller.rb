6ded7c3c9cddeef8549632a051347eb8466cedb8
prithajnath <prithajnath@gmail.com>
2016-07-06 19:08:46 -0400

    remove whitespaces

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -4,15 +4,10 @@ class PdfsController < ApplicationController
   end
 
   def new
-    
     temp_file = "#{::Rails.root}/public/mlhpdf"       
     Prawn::Document.generate("gendoc.pdf", :template => temp_file) do |f|
       send_data f.render,disposition:"inline"
-    	    
     end	
   end    
-
-
-
 
 end
